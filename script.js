4aec038a4057dfad3643018da0c459cde0447f71
JahanaviChaturvedi <118982130+JahanaviChaturvedi@users.noreply.github.com>
2025-03-27 21:48:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function highlight() {
-    //Write your code here
-
-
+    let boldWords = document.querySelectorAll('strong');
+    boldWords.forEach(word => {
+        word.style.color = "rgb(0, 128, 0)";
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
+    let boldWords = document.querySelectorAll('strong');
 
-    
+    boldWords.forEach(word => {
+        word.style.color = "rgb(0, 0, 0)";
+    });
 }
